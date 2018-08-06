eee4274c295f3dbd5f03efa868a17e173cd6d0de
ed murphy <edzillion@gmail.com>
2018-08-06 13:18:28 +0200

    load config at usertime

--- scripts/admin.js
+++ scripts/admin.js
@@ -7,16 +7,20 @@
 //    bot what's the time? - Tells you the time
 //
 module.exports = (robot) => {
+  function _initBotConfig () {
   // get configs from brain
-  var config = robot.brain.get('botConfig')
-  console.log('config loaded: ' + config)
-  if (!config) {
-    config = {
-      mode: 'prod' // can also be 'test'
+    var config = robot.brain.get('botConfig')
+    console.log('config loaded: ' + config)
+    if (!config) {
+      config = {
+        mode: 'prod' // can also be 'test'
+      }
     }
   }
 
   robot.respond(/set mode (prod|test)/i, (res) => {
+    _initBotConfig()
+
     let mode = res.match[1].toLowerCase()
     console.log('mode: ' + mode)
     if (config.mode === mode) res.reply('already in ' + mode + ' mode')
